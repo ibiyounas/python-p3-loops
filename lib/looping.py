ac4db24313c2436d1924e06d6d59c05098a8ba4b
Ibrahem Younas <younasibrahem@gmail.com>
2024-05-22 18:48:18 -0400

    passes tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
+    num = 10
+    while num > 0:
+        print(num)
+        num -= 1
+    print("Happy New Year!")
+    
+
     # code goes here!
     pass
 
 def square_integers(int_list):
+    sqrnum = [num **2 for num in int_list]
+    return(sqrnum)
     # code goes here!
     pass
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1,101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
     pass
